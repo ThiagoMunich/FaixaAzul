99496cdb3198d842240ac96ce476a9863b8c77c5
thiagomunich <thiagomunich@gmail.com>
2020-12-11 21:57:46 -0300

    Inserindo modal para cadastrar veiculos

--- src/pages/Home/ModalCadastrarVeiculo/index.js
+++ src/pages/Home/ModalCadastrarVeiculo/index.js
@@ -1,22 +1,36 @@
 import React from 'react';
 
-import { Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@material-ui/core'
 import TextField from '@material-ui/core/TextField'
 
 import ControleFormulario from '../../../components/ControleFormulario'
 
-function ModalCadastrarVeiculo() {
+function ModalCadastrarVeiculo({ abrir, fechar }) {
   return (
     <>
-      <Dialog>
+      <Dialog open={abrir} onClose={fechar}>
         <DialogTitle>
-          Cadastrar veículo
+          <Typography variant='h5'>
+            Cadastrar veículo
+          </Typography>
         </DialogTitle>
         <DialogContent>
           <ControleFormulario label='Placa'>
             <TextField variant='outlined' margin='dense' />
           </ControleFormulario>
+          <ControleFormulario label='Nome/apelido do veículo'>
+            <TextField variant='outlined' margin='dense' />
+          </ControleFormulario>
         </DialogContent>
+        <DialogActions>
+          <Button size='small'>
+            Cancelar
+          </Button>
+          <Button size='small'>
+            Salvar
+          </Button>
+        </DialogActions>
+
       </Dialog>
 
     </>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import Grid from "@material-ui/core/Grid"
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
@@ -7,56 +8,64 @@ import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import NotificationsNoneOutlinedIcon from '@material-ui/icons/NotificationsNoneOutlined';
 import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
 
-
 import Cartao from './Cartao'
+import ModalCadastrarVeiculo from './ModalCadastrarVeiculo'
 
 function Home() {
+
+  const [abrirCadastrarVeiculo, setAbrirCadastrarVeiculo] = useState(false)
+
+  const handleModalCadastrarVeiculo = () => {
+    setAbrirCadastrarVeiculo(!abrirCadastrarVeiculo)
+  }
+
   return (
     <>
-    <Grid container justify='space-between' alignContent='center' alignItems='center'>
-      <Grid item>
-        <div style={{display:'flex', alignContent:'center', alignItems:'center'}}>
-          
-        <Typography variant='h5' style={{marginRight:10}}>
-          Saldo:
+      <Grid container justify='space-between' alignContent='center' alignItems='center'>
+        <Grid item>
+          <div style={{ display: 'flex', alignContent: 'center', alignItems: 'center' }}>
+
+            <Typography variant='h5' style={{ marginRight: 10 }}>
+              Saldo:
         </Typography>
-        <Typography variant='body2'>
-          R$ 25,00 
+            <Typography variant='body2'>
+              R$ 25,00
         </Typography>
+          </div>
+        </Grid>
+        <Grid item>
+          <IconButton>
+            <AddShoppingCartIcon color='primary' />
+          </IconButton>
+        </Grid>
+      </Grid>
+      <Paper elevation={5} style={{ marginTop: 20, padding: 5 }}>
+        <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'space-between' }}>
+          <Typography variant='body2'>
+            Você tem 1 nova mensagem
+        </Typography>
+          <NotificationsNoneOutlinedIcon fontSize='small' color='secondary' />
         </div>
-      </Grid>
-      <Grid item>
-        <IconButton>
-          <AddShoppingCartIcon color='primary'/>
-        </IconButton>
-      </Grid>
-    </Grid>
-    <Paper elevation={5} style={{marginTop: 20, padding:5}}>
-      <div style={{display: 'flex', alignContent:'center', justifyContent:'space-between'}}>
-        <Typography variant='body2'>
-          Você tem 1 nova mensagem
-        </Typography>
-        <NotificationsNoneOutlinedIcon fontSize='small' color='secondary'/>
-      </div>
-    </Paper>
-    <Typography variant='h5' style={{marginTop: 60}}>
-      Veículos cadastrados
+      </Paper>
+      <Typography variant='h5' style={{ marginTop: 60 }}>
+        Veículos cadastrados
     </Typography>
-    <Grid container style={{marginTop:20}}>
-      <Cartao/>
-    </Grid>
-    <Grid container justify='center'>
-      <div style={{display:'flex', flexDirection:'column', alignItems:'center', marginTop: 70}}>
-      <IconButton >
-        <AddCircleOutlineOutlinedIcon color='primary'/>
-      </IconButton>
-      <Typography variant='body2'>
-        Cadastrar veículo
+      <Grid container style={{ marginTop: 20 }}>
+        <Cartao />
+      </Grid>
+      <Grid container justify='center'>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 70 }}>
+          <IconButton onClick={handleModalCadastrarVeiculo}>
+            <AddCircleOutlineOutlinedIcon color='primary' />
+          </IconButton>
+          <Typography variant='body2'>
+            Cadastrar veículo
       </Typography>
 
-      </div>
-    </Grid>
-   </> 
+        </div>
+      </Grid>
+      <ModalCadastrarVeiculo abrir={abrirCadastrarVeiculo} fechar={handleModalCadastrarVeiculo} />
+    </>
   );
 }
 
