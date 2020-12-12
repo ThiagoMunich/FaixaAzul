a786a484e01e25acb77853dd8f902eb990142077
thiagomunich <thiagomunich@gmail.com>
2020-12-12 11:55:43 -0300

    Iniciando o modal para comprar rotativo

---
+++ src/pages/Home/ModalComprarRotativo/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Dialog, DialogActions, DialogTitle, DialogContent, Button } from '@material-ui/core'
+
+function ModalComprarRotativo({ abrir, fechar }) {
+  return (
+    <>
+      <Dialog open={abrir} onClose={fechar}>
+        <DialogTitle>
+          Comprar rotativo
+        </DialogTitle>
+        <form onSubmit={handleSubmit}>
+          <DialogContent>
+            Compre aqui seu rotativo
+          </DialogContent>
+          <DialogActions>
+            <Button size='small' color='secondary' onClick={fechar}>
+              Cancelar
+          </Button>
+            <Button type='submit' size='small' color='primary'>
+              Comprar
+          </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+
+    </>
+  )
+}
+
+export default ModalComprarRotativo;
