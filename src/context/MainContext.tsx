d45e5bd76d0ce67d137608dc05c24b878b63ccb0
thiagomunich <thiagomunich@gmail.com>
2020-12-15 21:45:03 -0300

    Criando MainContext

---
+++ src/context/MainContext.tsx
@@ -0,0 +1,45 @@
+import React, {useState, createContext, useEffect} from 'react';
+
+interface ISaldo {
+  saldo: number;
+}
+
+interface IVeiculos {
+  nome: string,
+  placa: string,
+  tipo: number,
+}
+
+interface IMainContext {
+  saldo: ISaldo;
+  setSaldo: (saldo: ISaldo) => void;
+  tema: 'dark' | 'light';
+  trocarTema: () => void;
+}
+
+export const MainContext = createContext<IMainContext>({} as IMainContext)
+
+const MainProvider: React.FC = ({children}) => {
+  
+  const [tema,setTema] = useState<'dark' | 'light'>('light')
+
+  const [saldo, setSaldo] = useState<ISaldo>({} as ISaldo)
+
+  useEffect(() => {
+
+    if(localStorage.getItem('@FaixaAzul-Tema') !== null) {
+      setTema(localStorage.getItem('@FaixaAzul-Tema') === 'dark' ? 'dark' : 'light')
+    }
+
+  },[])
+
+  function trocarTema() {
+    setTema(tema === 'dark' ? 'light' : 'dark');
+  localStorage.setItem('@FaixaAzul-Tema', tema === 'dark' ? 'light' : 'dark');
+  }
+
+  return <MainContext.Provider value={{saldo,setSaldo, tema, trocarTema}}>{children}</MainContext.Provider>
+  
+}
+
+export default MainProvider;
