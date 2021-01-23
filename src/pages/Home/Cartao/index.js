f53120b31e5ee5311c637b55ba8c8a4b438a060a
thiagomunich <thiagomunich@gmail.com>
2021-01-23 09:32:16 -0300

    Removendo confirmação da placa do veiculo

--- src/pages/Home/Cartao/index.js
+++ src/pages/Home/Cartao/index.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 
 import Button from '@material-ui/core/Button';
-import Divider from "@material-ui/core/Divider"
+import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -16,59 +16,63 @@ import LocalShippingOutlinedIcon from '@material-ui/icons/LocalShippingOutlined'
 import SportsMotorsportsOutlinedIcon from '@material-ui/icons/SportsMotorsportsOutlined';
 
 const useStyles = makeStyles({
-  root: {
-    width: '100%',
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 12,
-    fontWeight: 700
-  },
-  pos: {
-    marginBottom: 12,
-  },
+	root: {
+		width: '100%',
+	},
+	bullet: {
+		display: 'inline-block',
+		margin: '0 2px',
+		transform: 'scale(0.8)',
+	},
+	title: {
+		fontSize: 12,
+		fontWeight: 700,
+	},
+	pos: {
+		marginBottom: 12,
+	},
 });
 
 export default function Cartao({ veiculo }) {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <Card className={classes.root} elevation={5}>
-      <CardContent>
-        <Typography variant='overline' className={classes.title}>
-          {veiculo.nome}
-        </Typography>
-        <Divider variant='fullWidth' />
-        <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'space-between', padding: 5, marginTop: 10 }}>
-          <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'flex-start', marginTop: 10 }}>
-            {veiculo.tipo === 1 ? (
-              <DirectionsCarOutlinedIcon style={{ marginRight: 10 }} />
-            ) : veiculo.tipo === 2 ? (
-              <LocalShippingOutlinedIcon style={{ marginRight: 10 }} />
-            ) : (
-                  <SportsMotorsportsOutlinedIcon style={{ marginRight: 10 }} />
-                )}
-            <Typography>
-              {veiculo.placa.toUpperCase()}
-            </Typography>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Typography variant='caption'>
-              +2 bônus!
-            </Typography>
-            <CardGiftcardOutlinedIcon color="secondary" />
-          </div>
-        </div>
-      </CardContent>
-      <CardActions>
-        <Button variant='outlined' fullWidth size='small' color='primary'>
-          Estacionar
-        </Button>
-      </CardActions>
-    </Card>
-  );
+	return (
+		<Card className={classes.root} elevation={5}>
+			<CardContent>
+				<Typography variant='overline' className={classes.title}>
+					{veiculo.nome}
+				</Typography>
+				<Divider variant='fullWidth' />
+				<div
+					style={{
+						display: 'flex',
+						alignContent: 'center',
+						justifyContent: 'space-between',
+						padding: 5,
+						marginTop: 10,
+					}}
+				>
+					<div style={{ display: 'flex', alignContent: 'center', justifyContent: 'flex-start', marginTop: 10 }}>
+						{veiculo.tipo === 1 ? (
+							<DirectionsCarOutlinedIcon style={{ marginRight: 10 }} />
+						) : veiculo.tipo === 2 ? (
+							<LocalShippingOutlinedIcon style={{ marginRight: 10 }} />
+						) : (
+							<SportsMotorsportsOutlinedIcon style={{ marginRight: 10 }} />
+						)}
+						<Typography>{veiculo.placa.toUpperCase()}</Typography>
+					</div>
+					<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+						<Typography variant='caption'>+2 bônus!</Typography>
+						<CardGiftcardOutlinedIcon color='secondary' />
+					</div>
+				</div>
+			</CardContent>
+			<CardActions>
+				<Button variant='outlined' fullWidth size='small' color='primary' onClick={() => console.log(veiculo)}>
+					Estacionar
+				</Button>
+			</CardActions>
+		</Card>
+	);
 }
--- src/pages/Home/ModalCadastrarVeiculo/index.js
+++ src/pages/Home/ModalCadastrarVeiculo/index.js
@@ -1,114 +1,124 @@
 import React, { useState } from 'react';
 
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@material-ui/core'
-import TextField from '@material-ui/core/TextField'
-import ButtonGroup from '@material-ui/core/ButtonGroup'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
-import { FormControl, FormGroup } from '@material-ui/core'
+import { FormControl, FormGroup } from '@material-ui/core';
 
 import DriveEtaIcon from '@material-ui/icons/DriveEta';
 import LocalShippingIcon from '@material-ui/icons/LocalShipping';
 import SportsMotorsportsIcon from '@material-ui/icons/SportsMotorsports';
 
-import { useFormik } from 'formik'
+import { useFormik } from 'formik';
 
 function ModalCadastrarVeiculo({ abrir, fechar, cadastrar }) {
+	const [carro, setCarro] = useState(true);
+	const [moto, setMoto] = useState(false);
+	const [caminhao, setCaminhao] = useState(false);
 
-  const [carro, setCarro] = useState(true)
-  const [moto, setMoto] = useState(false)
-  const [caminhao, setCaminhao] = useState(false)
+	const [tipoVeiculo, setTipoVeiculo] = useState('Automóvel');
+	const [tipoIcone, setTipoIcone] = useState(0);
 
-  const [tipoVeiculo, setTipoVeiculo] = useState('Automóvel')
-  const [tipoIcone, setTipoIcone] = useState(0);
+	const handleCarro = () => {
+		setCarro(true);
+		setTipoVeiculo('Automóvel');
+		setTipoIcone(1);
+		setMoto(false);
+		setCaminhao(false);
+	};
 
-  const handleCarro = () => {
-    setCarro(true)
-    setTipoVeiculo('Automóvel')
-    setTipoIcone(1)
-    setMoto(false)
-    setCaminhao(false)
-  }
+	const handleCaminhao = () => {
+		setCaminhao(true);
+		setTipoVeiculo('Carga e descarga');
+		setTipoIcone(2);
+		setCarro(false);
+		setMoto(false);
+	};
 
-  const handleCaminhao = () => {
-    setCaminhao(true)
-    setTipoVeiculo('Carga e descarga')
-    setTipoIcone(2)
-    setCarro(false)
-    setMoto(false)
-  }
+	const handleMoto = () => {
+		setMoto(true);
+		setTipoVeiculo('Motofrete C/D');
+		setTipoIcone(3);
+		setCarro(false);
+		setCaminhao(false);
+	};
 
-  const handleMoto = () => {
-    setMoto(true)
-    setTipoVeiculo('Motofrete C/D')
-    setTipoIcone(3)
-    setCarro(false)
-    setCaminhao(false)
-  }
+	const { handleSubmit, getFieldProps, resetForm } = useFormik({
+		initialValues: {
+			nome: '',
+			placa: '',
+			tipo: 0,
+		},
+		onSubmit: values => {
+			values.tipo = tipoIcone;
+			cadastrar(values);
+			resetForm();
+			setTimeout(fechar, 1);
+		},
+	});
 
-  const { handleSubmit, getFieldProps, resetForm } = useFormik({
-    initialValues: {
-      nome: '',
-      placa: '',
-      tipo: 0,
-    },
-    onSubmit: values => {
-      values.tipo = tipoIcone
-      cadastrar(values)
-      resetForm()
-      setTimeout(fechar, 1)
-    }
-  })
-
-  return (
-    <>
-      <Dialog open={abrir} onClose={fechar}>
-        <DialogTitle>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <ButtonGroup fullWidth>
-              <Button onClick={handleCarro}><DriveEtaIcon /></Button>
-              <Button onClick={handleCaminhao}><LocalShippingIcon /></Button>
-              <Button onClick={handleMoto}><SportsMotorsportsIcon /></Button>
-            </ButtonGroup>
-          </div>
-          <div style={{ display: 'flex', justifyContent: 'center', marginTop: 20 }}>
-            <Typography variant='overline'>
-              {tipoVeiculo}
-            </Typography>
-          </div>
-
-        </DialogTitle>
-        <form onSubmit={handleSubmit}>
-          <DialogContent>
-            <FormControl fullWidth>
-              <FormGroup>
-                <TextField variant='outlined' margin='dense' label='Nome' autoComplete='off' required {...getFieldProps('nome')} />
-              </FormGroup>
-            </FormControl>
-            <FormControl fullWidth>
-              <FormGroup>
-                <TextField variant='outlined' margin='dense' label='Placa' autoComplete='off' required {...getFieldProps('placa')} />
-              </FormGroup>
-            </FormControl>
-            <br />
-            <FormControl fullWidth>
-              <FormGroup>
-                <TextField variant='outlined' margin='dense' label='Confirme a placa' autoComplete='off' required {...getFieldProps('placa')} />
-              </FormGroup>
-            </FormControl>
-          </DialogContent>
-          <DialogActions>
-            <Button size='small' color='secondary' onClick={fechar}>
-              Cancelar
-          </Button>
-            <Button type='submit' size='small' color='primary'>
-              Salvar
-          </Button>
-          </DialogActions>
-        </form>
-      </Dialog>
-
-    </>
-  )
+	return (
+		<>
+			<Dialog open={abrir} onClose={fechar}>
+				<DialogTitle>
+					<div style={{ display: 'flex', alignItems: 'center' }}>
+						<ButtonGroup fullWidth>
+							<Button onClick={handleCarro}>
+								<DriveEtaIcon />
+							</Button>
+							<Button onClick={handleCaminhao}>
+								<LocalShippingIcon />
+							</Button>
+							<Button onClick={handleMoto}>
+								<SportsMotorsportsIcon />
+							</Button>
+						</ButtonGroup>
+					</div>
+					<div style={{ display: 'flex', justifyContent: 'center', marginTop: 20 }}>
+						<Typography variant='overline'>{tipoVeiculo}</Typography>
+					</div>
+				</DialogTitle>
+				<form onSubmit={handleSubmit}>
+					<DialogContent>
+						<FormControl fullWidth>
+							<FormGroup>
+								<TextField
+									required
+									margin='dense'
+									autoComplete='off'
+									variant='outlined'
+									label='Nome do veículo'
+									{...getFieldProps('nome')}
+								/>
+							</FormGroup>
+						</FormControl>
+						<FormControl fullWidth>
+							<FormGroup>
+								<TextField
+									required
+									label='Placa'
+									margin='dense'
+									autoComplete='off'
+									variant='outlined'
+									{...getFieldProps('placa')}
+								/>
+							</FormGroup>
+						</FormControl>
+						<br />
+					</DialogContent>
+					<DialogActions>
+						<Button size='small' color='secondary' onClick={fechar}>
+							Cancelar
+						</Button>
+						<Button type='submit' size='small' color='primary'>
+							Salvar
+						</Button>
+					</DialogActions>
+				</form>
+			</Dialog>
+		</>
+	);
 }
 
 export default ModalCadastrarVeiculo;
